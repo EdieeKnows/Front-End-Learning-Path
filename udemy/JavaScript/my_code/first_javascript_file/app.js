050ef0e95193900e0c8eaadeb67a9ef73281dfe2
EdieeKnows <Xu_yizhi@outlook.com>
2023-06-01 21:27:04 +1000

    update javascript learning material

---
+++ udemy/JavaScript/my_code/first_javascript_file/app.js
@@ -0,0 +1,31 @@
+console.log("Hello world!");
+const dayOfWork = "Friday";
+
+//if (dayOfWork === "Monday"){
+//	console.log("Today is Monday!!");
+//} else if (dayOfWork === "Friday") {
+//	console.log("YAY I love firday.");
+//}
+//
+//const age = parseInt(prompt("Enter your age: "));
+//
+//console.log("Your age is setted to %s", age);
+//
+//if (age < 5) {
+//	console.log("You are a child");
+//} else if (age < 10) {
+//	console.log("You are from 5-10");
+//} else if (age < 65) {
+//	console.log("You are an adult.");
+//} else {
+//	console.log("You are a senior.");
+//}
+
+while (1) {
+	let data = prompt("Just type something here: ");
+	if (data) {
+		console.log("This is a true.");
+	} else {
+		console.log("This is a false.");
+	}
+}
